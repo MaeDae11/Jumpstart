b26352e2bb70adeb470981e650ca999df0975d10
Katie <katherinelane11@gmail.com>
2017-07-26 10:50:14 -0400

    centers jumbotron text

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
       pauseOnHover: false,
       swipeToSlide: true,
   });
+
+  // var x = 0;
+  // var y = $(this).scrollTop();
+  // window.scroll(x, y);
+  // if (y - x > 50) {
+  //   var z = $('.navigation-bar').css('height');
+  //  $('.navbar').animate({top: '-' + z}, 150);
+  // }
+
 });
+
